24ae045c2930c0aa8c4062c7b347eeef42d4d7d9
CaptainDilawar <dilawarakram13@gmail.com>
2025-08-04 12:43:05 +0500

    before server changes

--- streamlit_start.sh
+++ streamlit_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Streamlit
-exec streamlit run dashboard/Home.py \
+exec /home/opc/.local/bin/streamlit run dashboard/Home.py \
   --server.port 8501 \
   --server.address 0.0.0.0 \
   --server.enableCORS false \
